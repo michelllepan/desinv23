14f8bd382f5aa5f28ba873b01f52ce822f2d7f5d
Michelle Pan <michellepan8@gmail.com>
2024-01-30 14:25:30 -0800

    Add reset button

--- ExpressiveLight/ExpressiveLight.ino
+++ ExpressiveLight/ExpressiveLight.ino
@@ -15,34 +15,39 @@ int blue = 0;
 int delaytime = 30;  // a variable for all delay times
                       // now we can change them all just by changing this one number!
 
-
 void setup(){
   pinMode(redLED, OUTPUT);
   pinMode(greenLED, OUTPUT);
   pinMode(blueLED, OUTPUT);
 }
 
-// note: full brightness is REALLY bright, so we often start at 50% and increase from there if necessary 
-
 void loop(){
+    int sensorValue = analogRead(A0);
     int redButtonState = digitalRead(redButtonPin);
     int greenButtonState = digitalRead(greenButtonPin);
     int blueButtonState = digitalRead(blueButtonPin);
 
+    if (sensorValue < 100) {
+      red = 0;
+      green = 0;
+      blue = 0;
+    }
+
     if (redButtonState == HIGH) {
       red += 1;
-      analogWrite(redLED, red % 128);
     }
 
     if (greenButtonState == HIGH) {
       green += 1;
-      analogWrite(greenLED, green % 128);
     }
 
     if (blueButtonState == HIGH) {
       blue += 1;
-      analogWrite(blueLED, blue % 128);
     }
+
+    analogWrite(redLED, red % 128);
+    analogWrite(greenLED, green % 128);
+    analogWrite(blueLED, blue % 128);
 
     delay(delaytime);
 }
