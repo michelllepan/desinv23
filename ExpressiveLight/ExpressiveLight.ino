aeadd4661c9a34ed6f72ef81154d6f3975242da2
Michelle Pan <michellepan8@gmail.com>
2024-02-13 20:21:41 -0800

    Add comments

--- ExpressiveLight/ExpressiveLight.ino
+++ ExpressiveLight/ExpressiveLight.ino
@@ -1,82 +1,96 @@
 
 // LED pins
-const int redLED = 11;  
-const int greenLED = 10;  
-const int blueLED = 9;  
+const int redLED = 11;
+const int greenLED = 10;
+const int blueLED = 9;
 
+// button pins
 const int redButtonPin = 4;
 const int greenButtonPin = 3;
 const int blueButtonPin = 2;
 
-int redFadeAmount = 1;    
-int greenFadeAmount = 5;    
-int blueFadeAmount = 3;  
+// threshold where LDR is considered "covered"
+const int ldrThreshold = 100;
 
+// amount to fade each color when in fade mode
+int redFadeAmount = 1;
+int greenFadeAmount = 5;
+int blueFadeAmount = 3;
+
+// variables for RGB color values
 int red = 0;
-int green = 0; 
+int green = 0;
 int blue = 0;
 
-int delaytime = 30;  // a variable for all delay times
-                      // now we can change them all just by changing this one number!
+int delaytime = 30;
 
-void setup(){
+void setup() {
   pinMode(redLED, OUTPUT);
   pinMode(greenLED, OUTPUT);
   pinMode(blueLED, OUTPUT);
 }
 
-void loop(){
-    int sensorValue = analogRead(A0);
-    int redButtonState = digitalRead(redButtonPin);
-    int greenButtonState = digitalRead(greenButtonPin);
-    int blueButtonState = digitalRead(blueButtonPin);
+void loop() {
+  int sensorValue = analogRead(A0);
+  int redButtonState = digitalRead(redButtonPin);
+  int greenButtonState = digitalRead(greenButtonPin);
+  int blueButtonState = digitalRead(blueButtonPin);
 
-    if (sensorValue < 100) {
-        fade();
-    } else {
-        adjustColor();
-    }
+  // if LDR is covered, fade LED; else adjust based on buttons
+  if (sensorValue < ldrThreshold) {
+    fade();
+  } else {
+    adjustColor();
+  }
 
-    analogWrite(redLED, red);
-    analogWrite(greenLED, green);
-    analogWrite(blueLED, blue);
+  analogWrite(redLED, red);
+  analogWrite(greenLED, green);
+  analogWrite(blueLED, blue);
 
-    delay(delaytime);
+  delay(delaytime);
 }
 
-void fade(){
-    red += redFadeAmount;
-    if (red < 1 || red > 199) {
-        redFadeAmount = -redFadeAmount;
-    }
+// fade mode
+void fade() {
 
-    green += greenFadeAmount;
-    if (green < 5 || green > 195) {
-        greenFadeAmount = -greenFadeAmount;
-    }
+  // fade colors by their fade amount between 0 and 200
+  
+  red += redFadeAmount;
+  if (red < 1 || red > 199) {
+    redFadeAmount = -redFadeAmount;
+  }
 
-    blue += blueFadeAmount;
-    if (blue < 3 || blue > 197) {
-        blueFadeAmount = -blueFadeAmount;
-    }
+  green += greenFadeAmount;
+  if (green < 5 || green > 195) {
+    greenFadeAmount = -greenFadeAmount;
+  }
 
-    delay(delaytime);
+  blue += blueFadeAmount;
+  if (blue < 3 || blue > 197) {
+    blueFadeAmount = -blueFadeAmount;
+  }
+
+  delay(delaytime);
 }
 
+// manual adjustment from buttons
 void adjustColor() {
-    int redButtonState = digitalRead(redButtonPin);
-    int greenButtonState = digitalRead(greenButtonPin);
-    int blueButtonState = digitalRead(blueButtonPin);
+  int redButtonState = digitalRead(redButtonPin);
+  int greenButtonState = digitalRead(greenButtonPin);
+  int blueButtonState = digitalRead(blueButtonPin);
 
-    if (redButtonState == HIGH) {
-      red = (red + 2) % 200;
-    }
+  // if the button for a color is pressed down, increment the
+  // color value and loop back to 0 if it exceeds 200
 
-    if (greenButtonState == HIGH) {
-      green = (green + 2) % 200;
-    }
+  if (redButtonState == HIGH) {
+    red = (red + 2) % 200;
+  }
 
-    if (blueButtonState == HIGH) {
-      blue = (blue + 2) % 200;
-    }
+  if (greenButtonState == HIGH) {
+    green = (green + 2) % 200;
+  }
+
+  if (blueButtonState == HIGH) {
+    blue = (blue + 2) % 200;
+  }
 }
